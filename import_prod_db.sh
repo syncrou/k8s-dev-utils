bc526ca80f25e89bbf4710dd77d059de4ba6019c
Jacob Lindgren <jlindgre@redhat.com>
2020-01-30 12:48:52 -0600

    add resource for database in own namespace and add initContainer to catalog deployment to import ci contents

--- import_prod_db.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-RUNNER=kubectl # change to oc if you want to use that
-DB_POD=$($RUNNER get po | awk '/postgresql/ {print $1}')
-
-$RUNNER exec -t $DB_POD -- \
-    bash -c "/opt/rh/rh-postgresql10/root/usr/bin/pg_dump catalog_production --clean" | \
-    sed 's/catalog_production/catalog_development/g' | \
-    bin/rails dbconsole -p
