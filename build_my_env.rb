85508f3013d868cf42dc6a1541aff37b45888ea0
Drew Bomhof <dbomhof@redhat.com>
2020-02-03 15:33:31 -0500

    Modify persist_refs with the namespace

--- build_my_env.rb
+++ build_my_env.rb
@@ -32,21 +32,34 @@ system("./copy_catalog_db_secret.sh")
 puts("Creating the local ephemeral postgres db")
 system("oc create -f ./database.yml")
 
-puts "Creating the builds for catalog and minion"
+puts "Creating the builds for catalog"
 system("./create_build.rb --repo #{opts[:repo]} --branch #{opts[:branch]}")
 sleep(20)
 
-puts "Editing the deployment files with branch: #{opts[:branch]}"
+puts "Editing the catalog deployment files with branch: #{opts[:branch]}"
 catalog = YAML.load_file "configs/catalog.yml"
 catalog['spec']['template']['spec']['containers'].first['image'] = "docker-registry.default.svc:5000/#{opts[:namespace]}/catalog-api"
 File.open("configs/catalog-custom.yml", 'w') { |f| YAML.dump(catalog, f) }
 
+puts "Appending the approval minion deployment persist_ref with namespace: #{opts[:namespace]}"
+approval = YAML.load_file "configs/approval-minion.yml"
+approval['spec']['template']['spec']['containers'].first['env'][5]['value'] = "test-approval-persist-ref-#{opts[:namespace]}"
+File.open("configs/approval-minion-custom.yml", 'w') { |f| YAML.dump(approval, f) }
+
+puts "Appending the task minion deployment persist_ref with namespace: #{opts[:namespace]}"
+task = YAML.load_file "configs/task-minion.yml"
+task['spec']['template']['spec']['containers'].first['env'][5]['value'] = "test-task-persist-ref-#{opts[:namespace]}"
+File.open("configs/task-minion-custom.yml", 'w') { |f| YAML.dump(task, f) }
+
 puts "creating the deployment and service for catalog and the minions"
 system("oc create -f configs/catalog-custom.yml")
-system("oc create -f ./minions.yml")
+system("oc create -f configs/approval-minion-custom.yml")
+system("oc create -f configs/task-minion-custom.yml")
 sleep(20)
 
 puts "Cleaning up the temp deployment files"
 File.delete("configs/catalog-custom.yml")
+File.delete("configs/approval-minion-custom.yml")
+File.delete("configs/task-minion-custom.yml")
 
 puts "Done"
