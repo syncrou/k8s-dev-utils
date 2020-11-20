680c335740ce4095ba420863895554f7bbae9ea0
Drew Bomhof <dbomhof@redhat.com>
2020-11-20 07:24:56 -0500

    Adding personal env tweaks

--- dev_kube.rb
+++ dev_kube.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+#!/home/dbomhof/.rubies/ruby-2.5.7/bin/ruby
 # frozen_string_literal: true
 
 require 'rubygems'
@@ -9,10 +9,11 @@ Bundler.require
 require 'kubeclient'
 require 'filewatcher'
 
-src = ENV['SRC']
-dest = ENV['DEST']
-namespace = ENV['NAMESPACE']
-svc = ENV['POD']
+src = ENV['SRC'] || "/home/dbomhof/onramp"
+dest = ENV['DEST'] || "/var/www/onramp/"
+namespace = ENV['NAMESPACE'] || "cdg"
+svc = ENV['POD'] || "onramp"
+skip_tls = { :verify_ssl => false }
 
 config = Kubeclient::Config.read(
   ENV['KUBECONFIG'] || "#{ENV['HOME']}/.kube/config"
@@ -21,16 +22,23 @@ config = Kubeclient::Config.read(
 @kube = Kubeclient::Client.new(
   config.context.api_endpoint,
   'v1',
-  ssl_options: config.context.ssl_options,
+  ssl_options: config.context.ssl_options.merge!(skip_tls),
   auth_options: config.context.auth_options
 )
-
-pod = @kube.get_pods(
-  namespace: namespace, label_selector: "app=#{svc}"
-).first.metadata.name
+if namespace == "cdg"
+  pod = @kube.get_pods(
+    namespace: namespace, label_selector: "app=#{svc}"
+  ).first.metadata.name
+else
+  puts "NAMESPACE = #{namespace}"
+  pod = @kube.get_pods(
+    namespace: namespace, label_selector: "run=#{svc}"
+  ).first.metadata.name
+end
 
 puts('syncing...')
-system("oc --namespace #{namespace} rsync #{src} #{pod}:#{dest}")
+#system("oc --namespace #{namespace} rsync #{src} #{pod}:#{dest}")
+system("oc --insecure-skip-tls-verify=true --namespace #{namespace} rsync #{src} #{pod}:#{dest}")
 
 Dir.chdir(src)
 
@@ -45,6 +53,7 @@ Filewatcher.new(['./**/*.*', './'],
   when :created, :updated
     puts "Transferring #{local_path}... "
     # " -> kubectl -n #{namespace} cp #{local_path} #{pod}:#{dest}#{local_path}"
-    `kubectl -n #{namespace} cp #{local_path} #{pod}:#{dest}#{local_path}`
+    #`kubectl -n #{namespace} cp #{local_path} #{pod}:#{dest}#{local_path}`
+    `kubectl --insecure-skip-tls-verify -n #{namespace} cp #{local_path} #{pod}:#{dest}#{local_path}`
   end
 end
